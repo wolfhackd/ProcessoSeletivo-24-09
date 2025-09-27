73653fce1763db381fb76bc3ac29c56bfe11d9bb
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-09-27 01:59:03 -0300

    Componente CardAlocacao Criado

---
+++ frontend/components/CardAlocacao.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React from 'react';
+import { Button } from './ui/button';
+import { Card, CardContent } from './ui/card';
+import { AlertTriangleIcon, MoreVertical, PencilIcon } from 'lucide-react';
+import { Badge } from './ui/badge';
+
+const CardAlocacao = () => {
+  return (
+    <Card className="bg-[#1B1B1B] text-white mt-2 relative z-20 mb-4">
+      <CardContent className="px-2.5 flex justify-between items-center">
+        <div>
+          <div className="flex gap-2 items-center">
+            <h2>CDB Banco Itaú</h2>
+            <Badge className="bg-[#CFF8DF] text-[#408E37]">Financeira Manual</Badge>
+          </div>
+          <p>Inicio: 20/06/2023</p>
+        </div>
+        <div className="flex justify-between items-center gap-12">
+          <Button
+            variant={'outline'}
+            className="border-[#FD6C00] text-[#FD6C00] bg-[#F8F8F8] hover:text-[#FD6C00] hover:border-[#FD6C00] rounded-full h-9 w-36"
+          >
+            <PencilIcon /> Atualizar
+          </Button>
+          <div className="flex gap-4 items-center">
+            <div className="flex flex-col text-right">
+              <h2 className="text-xl">R$ 1.000.000</h2>
+              <span className="text-[#FD6C00] flex items-center gap-2 text-sm">
+                <AlertTriangleIcon className="size-4" /> Última atualização: 10/06/2023
+              </span>
+            </div>
+            <MoreVertical />
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default CardAlocacao;
--- frontend/pages/Alocacoes.tsx
+++ frontend/pages/Alocacoes.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import CardAlocacao from '@/components/CardAlocacao';
 import { FiltroAlocacoes } from '@/components/FiltroAlocacoes';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
@@ -20,37 +21,12 @@ export function Alocacoes() {
       </div>
       <div className="mt-10">
         <p className="text-muted-foreground">Dados Antigos</p>
-        <div className="relative py-2">
+        <div className="relative py-2 ">
           <div className="border-l-4 rounded-full border-muted-foreground left-4 h-full z-10 absolute" />
           {/* Lista de cards */}
-          <Card className="bg-[#1B1B1B] text-white mt-2 relative z-20">
-            <CardContent className="px-2.5 flex justify-between items-center">
-              <div>
-                <div className="flex gap-2 items-center">
-                  <h2>CDB Banco Itaú</h2>
-                  <Badge className="bg-[#CFF8DF] text-[#408E37]">Financeira Manual</Badge>
-                </div>
-                <p>Inicio: 20/06/2023</p>
-              </div>
-              <div className="flex justify-between items-center gap-12">
-                <Button
-                  variant={'outline'}
-                  className="border-[#FD6C00] text-[#FD6C00] bg-[#F8F8F8] hover:text-[#FD6C00] hover:border-[#FD6C00] rounded-full h-9 w-36"
-                >
-                  <PencilIcon /> Atualizar
-                </Button>
-                <div className="flex gap-4 items-center">
-                  <div className="flex flex-col text-right">
-                    <h2 className="text-xl">R$ 1.000.000</h2>
-                    <span className="text-[#FD6C00] flex items-center gap-2 text-sm">
-                      <AlertTriangleIcon className="size-4" /> Última atualização: 10/06/2023
-                    </span>
-                  </div>
-                  <MoreVertical />
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+          <CardAlocacao />
+          <CardAlocacao />
+          <CardAlocacao />
         </div>
         <p className="text-muted-foreground mt-2">Atualizado</p>
       </div>
